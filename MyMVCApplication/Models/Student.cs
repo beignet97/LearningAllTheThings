4b4fe8a1c36418bb0cb4c690df4541081338cf02
Reece Dudley <reece.dudley@x-labsystems.co.uk>
2021-08-20 16:56:51 +0100

    Corrected error with path for the shared layout page and implemented the 'Edit student' page with data validators

--- MyMVCApplication/Models/Student.cs
+++ MyMVCApplication/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,10 @@ namespace MyMVCApplication.Models
     public class Student
     {
         public int StudentId { get; set; }
-        public int StudentName { get; set; }
+        [Display(Name = "Name")]
+        [Required]
+        public string StudentName { get; set; }
+        [Range(10, 20)]
         public int Age { get; set; }
     }
 }
