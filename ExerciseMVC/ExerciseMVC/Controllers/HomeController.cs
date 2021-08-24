8140902c3a733fa3271348aab6e506c2dcd83d66
Reece Dudley <reece.dudley@x-labsystems.co.uk>
2021-08-24 15:44:57 +0100

    Edited default project structure to make finished to do list website

---
+++ ExerciseMVC/ExerciseMVC/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using ExerciseMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExerciseMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        static IList<Task> taskList = new List<Task>
+        {
+            new Task() { TaskId = 1, TaskDescription = "Example task" }
+        };
+
+        public ActionResult Index()
+        {
+            return View(taskList.ToList());
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection values)
+        {
+            var newTask = new Task() { TaskId = taskList.Count + 1, TaskDescription = values["TaskDescription"] };
+            taskList.Add(newTask);
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ ExerciseMVC/ExerciseMVC/Models/Task.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExerciseMVC.Models
+{
+    public class Task
+    {
+        public int TaskId { get; set; }
+        public string TaskDescription { get; set; }
+    }
+}
