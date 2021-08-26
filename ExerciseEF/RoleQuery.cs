e015a5ebfba4ff719298e60de9984e091c82d16a
Reece Dudley <reece.dudley@x-labsystems.co.uk>
2021-08-26 16:34:27 +0100

    Repurposed the program class to run a query to determine how many employees each role has.

---
+++ ExerciseEF/RoleQuery.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseEF
+{
+    class RoleQuery
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new EmployeeContext())
+            {
+                foreach (var role in db.Roles)
+                {
+                    Console.WriteLine(role.Name);
+
+                    var employees = db.Employees
+                                    .Where(e => e.Job.Name == role.Name)
+                                    .ToList()
+                                    .Count();
+
+                    Console.WriteLine(employees + " employees");
+                    Console.WriteLine();
+                }
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
