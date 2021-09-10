06e41ed5b9f557e334f5ccb8545494c6017600ea
Reece Dudley <reece.dudley@x-labsystems.co.uk>
2021-09-10 16:07:53 +0100

    Created baseline app and added a table for Castles along with a database context. Entity framework has been installed and migrations have been enabled. The Castles have been added to the seed method

---
+++ GameOfThrones/Migrations/Configuration.cs
@@ -0,0 +1,51 @@
+﻿namespace GameOfThrones.Migrations
+{
+    using GameOfThrones.Models;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<GameOfThrones.Models.CastleContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(GameOfThrones.Models.CastleContext context)
+        {
+            context.Castles.AddOrUpdate(i => i.Name,
+                new Castle
+                {
+                    Name = "The Eyrie"
+                },
+
+                new Castle
+                {
+                    Name = "Pyke"
+                },
+
+                new Castle
+                {
+                    Name = "Casterly Rock"
+                },
+
+                new Castle
+                {
+                    Name = "Winterfell"
+                },
+
+                new Castle
+                {
+                    Name = "The Twins"
+                },
+
+                new Castle
+                {
+                    Name = "Dragonstone"
+                }
+            );
+        }
+    }
+}
---
+++ GameOfThrones/Models/Castle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace GameOfThrones.Models
+{
+    public class Castle
+    {
+        public int CastleId { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class CastleContext : DbContext
+    {
+        public DbSet<Castle> Castles { get; set; }
+    }
+}
