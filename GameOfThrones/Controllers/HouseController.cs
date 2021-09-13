db94ec9bd0c853f30bedae73cbabbb57ae17cc43
Reece Dudley <reece.dudley@x-labsystems.co.uk>
2021-09-13 12:17:23 +0100

    Added a controller for the houses and a new page to view the houses in

---
+++ GameOfThrones/Controllers/HouseController.cs
@@ -0,0 +1,20 @@
+﻿using GameOfThrones.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GameOfThrones.Controllers
+{
+    public class HouseController : Controller
+    {
+        private CastleContext db = new CastleContext();
+
+        // GET: House
+        public ActionResult Index()
+        {
+            return View(db.Houses.ToList());
+        }
+    }
+}
