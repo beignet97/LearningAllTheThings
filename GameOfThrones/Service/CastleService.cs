54450b4bd774f588441a8f6175d7b8b496f2443a
Reece Dudley <reece.dudley@x-labsystems.co.uk>
2021-09-27 18:02:44 +0100

    Started refactoring code to use clean architecture with majortiy of castle changes made

---
+++ GameOfThrones/Service/CastleService.cs
@@ -0,0 +1,21 @@
+﻿using GameOfThrones.DAL;
+using GameOfThrones.Models;
+using GameOfThrones.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GameOfThrones.Service
+{
+    public class CastleService
+    {
+        private protected WikiContext _context;
+        private protected CastleRepository _repo;
+
+        public List<Castle> GetCastles() 
+        {
+            return _repo.GetAllCastles(_context);
+        }
+    }
+}
---
+++ ICastleRepository.cs
@@ -0,0 +1,14 @@
+﻿using GameOfThrones.Models;
+using System;
+
+public interface ICaslteRepository
+{
+	public ICaslteRepository()
+	{
+		List<Castle> GetAll();
+		Castle GetById(int id);
+		Add(Castle entity);
+		Update(Castle entity);
+		Delete(Castle entity);
+	}
+}
